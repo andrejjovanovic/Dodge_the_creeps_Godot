8812c579ec65aeb73991cff48ef89edbe35b940d
Andrej Jovanovic <andrej.jovanovic01@gmail.com>
2022-12-08 00:26:41 +0100

    Initial commit

---
+++ Main.gd
@@ -0,0 +1,28 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
