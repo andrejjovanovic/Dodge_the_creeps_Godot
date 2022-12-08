5b11142b30fa9fa12d8987d4784f0b1b45125314
Andrej Jovanovic <andrej.jovanovic01@gmail.com>
2022-12-08 18:33:37 +0100

    Finished game

--- Player.gd
+++ Player.gd
@@ -49,7 +49,7 @@ func _on_Player_body_entered(body):
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
 
-func _start(pos):
+func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
